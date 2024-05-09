4cb7b4708c37264e7474e3621011389dce1e8d45
ArfainSaba <arfainsabasaba@gmai.com>
2024-05-09 11:22:17 +0530

    added labels to metrics

--- dockerfile
+++ dockerfile
@@ -7,10 +7,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /home/app
 RUN pip install uvicorn
 
-# Copy the prometheus.yml file to the appropriate location
+
 COPY prometheus.yml /etc/prometheus/
 
-# Expose the port your application runs on (if necessary)
-# EXPOSE 8080
 
 CMD [ "uvicorn","main:app","--host","0.0.0.0","--port", "8001" ]
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from services.services import queries
 from fastapi import FastAPI, HTTPException,Response,Request
 from models import datasetsModel
 from typing import List
-import uvicorn 
+from prometheus_client.exposition import CONTENT_TYPE_LATEST
+import uvicorn
+import statistics 
+import requests
 
 
 conn_string = "host='db' port='5432' dbname='obsrv' user='obsrv_userr' password='obsrv123'"
@@ -16,32 +19,43 @@ print("connection successfull")
 
 app = FastAPI()
 
+api_requests_counter = Counter(
+    "api_requests_total",
+    "Total number of API calls",
+    ["api", "status", "request_size", "response_size", "response_time"]
+)
 
-total_api_calls = Counter('total_api_calls', 'Total number of API calls')
-successful_api_calls = Counter('successful_api_calls', 'Total number of successful API calls')
-failed_api_calls = Counter('failed_api_calls', 'Total number of failed API calls')
-total_response_time = Gauge('total_response_time', 'Total response time of all API calls')
-successful_response_time = Gauge('successful_response_time', 'Response time of successful API calls')
-failed_response_time = Gauge('failed_response_time', 'Response time of failed API calls')
-max_response_time = Gauge('max_response_time', 'Max response time of all API calls')
-min_response_time = Gauge('min_response_time', 'Min response time of all API calls')
+api_requests_gauge = Gauge(
+    "api_response_time",
+    "Total number of responses",
+    ["api", "status", "request_size", "response_size", "response_time"]
+)
 
+# api_requests_counter.labels(status="200").inc()
+# 4. Average response  time of all the API
+    # 5. Average response time of successful API
+    # 6. Average response time of failed API
 
 conn = connect(conn_string)
 cur= conn.cursor()
 cur.execute("SELECT * FROM datasets;")
 results=cur.fetchall()
+cur.close()
+conn.close()
+response_times = []
+successfull_response_time_array=[]
+failed_response_time_Array=[]
 
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    # total_response_time.inc(process_time)
-    total_response_time.set(process_time)
-    successful_response_time.set(process_time)
-    failed_response_time.set(process_time)
-    return response
+# @app.middleware("http")
+# async def add_process_time_header(request: Request, call_next):
+#     start_time = time.time()
+#     response = await call_next(request)
+#     process_time = time.time() - start_time
+#     # total_response_time.inc(process_time)
+#     # total_response_time.set(process_time)
+#     # successful_response_time.set(process_time)
+#     # failed_response_time.set(process_time)
+#     return response
 
 
 @app.get('/')
@@ -53,9 +67,17 @@ async def root():
 async def metrics():
     return Response(media_type="text/plain", content=generate_latest())
 
+
 @app.get("/v1/dataset")
 async def get_datasets() -> List[dict]:
-    total_api_calls.inc()
+    start_time = time.time()
+    # total_api_calls.inc()
+    
+    # api_requests_counter.labels(status="200").inc()
+    api_requests_counter.labels(api="app.get", status="", request_size="", response_size="", response_time="").inc()
+    
+
+    
     try:
         formatted_results = []
         for row in results:
@@ -80,13 +102,36 @@ async def get_datasets() -> List[dict]:
             "updated_date": row[17],
             "published_date": row[18]
         }
+        # successful_api_calls.inc()
+        api_requests_counter.labels(api="app.get", status="200", request_size="", response_size="", response_time="").inc()
+    
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.get", status="", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+        print("stuck")
         formatted_results.append(formatted_row)
-        successful_api_calls.inc()
         
     except:
-        failed_api_calls.inc()
+        # failed_api_calls.inc()
+        api_requests_counter.labels(api="app.get", status="404", request_size="", response_size="", response_time="").inc()
+    
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.get", status="404", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+
+    finally:
+        
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.get", status="", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+        
     return formatted_results
-    # return results
 
 
 
@@ -102,8 +147,12 @@ def get_item(item_id:str, description="id of the item"):
 
 @app.post("/v1/datasets")
 async def create_item(datasetsmodel:datasetsModel):
+    start_time = time.time()
     print("inside post")
-    total_api_calls.inc()
+    # total_api_calls.inc()
+    
+    api_requests_counter.labels(api="app.post", status="", request_size="", response_size="", response_time="").inc()
+    
     try:
         conn = connect(conn_string)
         cur = conn.cursor()
@@ -119,63 +168,157 @@ async def create_item(datasetsmodel:datasetsModel):
         datasetsmodel_dict['dataset_config'] = json.dumps(datasetsmodel_dict['dataset_config'])
 
         # print("Datasets Model Dictionary:", datasetsmodel_dict) 
+        # successful_api_calls.inc()
+        api_requests_counter.labels(api="app.post", status="200", request_size="", response_size="", response_time="").inc()
+    
         cur.execute(queries["create_dataset"], datasetsmodel_dict)
-        successful_api_calls.inc()
         print("incremented")
         conn.commit()
         cur.close()
         conn.close()
+        return {"message": "Dataset created successfully"}
     except:
-        failed_api_calls.inc()
-        return {"This Data already exists"}
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_counter.labels(api="app.post", status="500", request_size="", response_size="", response_time="").inc()
+    
+        api_requests_gauge.labels(api="app.post", status="500", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+    finally:
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.post", status="", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+        
+        # end_time = time.time()
+        # process_time = end_time - start_time
+        # response_times.append(process_time)
+        # total_response_time.set(process_time)
+        # minrst= min(response_times)
+        # min_response_time.set(minrst)
+        # maxrst= max(response_times)
+        # max_response_time.set(maxrst)
+        # avg_response = statistics.mean(response_times)
+        # avg_response_time.set(avg_response)
+
+    return {"This Data already exists"}
     
     
-    return {"message": "Dataset created successfully"}
+    
 
 @app.patch("/v1/datasets/{item_id}")
 async def update_dataset(item_id:str, datasetsmodel:datasetsModel):
-    conn=connect(conn_string)
-    cur=conn.cursor()
-
-    cur.execute(queries["get_item"], (item_id,))
-    existing_dataset = cur.fetchone()
-    if not existing_dataset:
-        raise HTTPException(status_code=404, detail="Dataset not found")
+    # total_api_calls.inc()
+    start_time = time.time()
+    api_requests_counter.labels(api="app.patch", status="", request_size="", response_size="", response_time="").inc()
     
-    datasetsmodel_dict = datasetsmodel.dict()
+    try:
+        conn=connect(conn_string)
+        cur=conn.cursor()
 
-    datasetsmodel_dict['validation_config'] = json.dumps(datasetsmodel_dict['validation_config'])
-    datasetsmodel_dict['extraction_config'] = json.dumps(datasetsmodel_dict['extraction_config'])
-    datasetsmodel_dict['dedup_config'] = json.dumps(datasetsmodel_dict['dedup_config'])
-    datasetsmodel_dict['data_schema'] = json.dumps(datasetsmodel_dict['data_schema'])
-    datasetsmodel_dict['denorm_config'] = json.dumps(datasetsmodel_dict['denorm_config'])
-    datasetsmodel_dict['router_config'] = json.dumps(datasetsmodel_dict['router_config'])
-    datasetsmodel_dict['dataset_config'] = json.dumps(datasetsmodel_dict['dataset_config'])
+        cur.execute(queries["get_item"], (item_id,))
+        existing_dataset = cur.fetchone()
+        if not existing_dataset:
+            raise HTTPException(status_code=404, detail="Dataset not found")
+    
+        datasetsmodel_dict = datasetsmodel.dict()
 
-    cur.execute(queries["update_dataset"], datasetsmodel_dict)
+        datasetsmodel_dict['validation_config'] = json.dumps(datasetsmodel_dict['validation_config'])
+        datasetsmodel_dict['extraction_config'] = json.dumps(datasetsmodel_dict['extraction_config'])
+        datasetsmodel_dict['dedup_config'] = json.dumps(datasetsmodel_dict['dedup_config'])
+        datasetsmodel_dict['data_schema'] = json.dumps(datasetsmodel_dict['data_schema'])
+        datasetsmodel_dict['denorm_config'] = json.dumps(datasetsmodel_dict['denorm_config'])
+        datasetsmodel_dict['router_config'] = json.dumps(datasetsmodel_dict['router_config'])
+        datasetsmodel_dict['dataset_config'] = json.dumps(datasetsmodel_dict['dataset_config'])
+        cur.execute(queries["update_dataset"], datasetsmodel_dict)
+        # successful_api_calls.inc()
+        api_requests_counter.labels(api="app.patch", status="200", request_size="", response_size="", response_time="").inc()
+    
+
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.patch", status="200", request_size="", response_size="", response_time="").set(process_time * 1000)
 
 
-    conn.commit()
-    cur.close()
-    conn.close()
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    except:
+        api_requests_counter.labels(api="app.patch", status="500", request_size="", response_size="", response_time="").inc()
+    
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.patch", status="500", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+
+    finally:
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.patch", status="200", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+
 
     return {"message": "Dataset updated successfully"}
 
 @app.delete("/v1/datasets/{item_id}")
 def delete_dataset(item_id: str):
-    conn = connect(conn_string)
-    cur = conn.cursor()
+    start_time = time.time()
+    # total_api_calls.inc()
+    api_requests_counter.labels(api="app.delete", status="", request_size="", response_size="", response_time="").inc()
+    
+    try:
+        conn = connect(conn_string)
+        cur = conn.cursor()
 
-    cur.execute(queries["get_item"], (item_id,))
-    result = cur.fetchone()
-    if result is None:
-        raise HTTPException(status_code=404, detail="Dataset not found")
+        cur.execute(queries["get_item"], (item_id,))
+        result = cur.fetchone()
+        if result is None:
+            raise HTTPException(status_code=404, detail="Dataset not found")
 
-    cur.execute(queries["delete_dataset"], (item_id,))
-    conn.commit()
+        cur.execute(queries["delete_dataset"], (item_id,))
+        
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.delete", status="200", request_size="", response_size="", response_time="").set(process_time * 1000)
 
-    cur.close()
-    conn.close()
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+    except:
+        api_requests_counter.labels(api="app.delete", status="500", request_size="", response_size="", response_time="").inc()
+    
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.delete", status="500", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+
+    finally:
+        end_time = time.time()
+        process_time = end_time - start_time
+        response_times.append(process_time)
+        api_requests_gauge.labels(api="app.delete", status="", request_size="", response_size="", response_time="").set(process_time * 1000)
+
+        # end_time = time.time()
+        # process_time = end_time - start_time
+        # response_times.append(process_time)
+        # total_response_time.set(process_time)
+        # minrst= min(response_times)
+        # min_response_time.set(minrst)
+        # maxrst= max(response_times)
+        # max_response_time.set(maxrst)
+        # avg_response = statistics.mean(response_times)
+        # avg_response_time.set(avg_response)
     return {"message":"Dataset deleted"}
 
 cur.close()
